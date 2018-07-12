acad0d50ed86da1d64e7228daec1b094a2ed5048
Rick Twohy <twohyjr@gmail.com>
2018-07-12 16:53:41 -0500

    Initial Commit

---
+++ The Dark Place/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  The Dark Place
+//
+//  Created by Twohy on 7/12/18.
+//  Copyright © 2018 TwohyTutorials. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
