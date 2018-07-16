2d8e4fe9d3f6d75edf65cdf4e5d68011bb01502c
Rick Twohy <twohyjr@gmail.com>
2018-07-15 22:33:07 -0500

    Refactored Meshes

---
+++ The Dark Place/Libraries/New Group/CustomMeshes/Quad_CustomMesh.swift
@@ -0,0 +1,24 @@
+import MetalKit
+
+class Quad_CustomMesh: CustomMesh {
+    //Origin: Bottom Left
+    override func createVertices() {
+        //Top Right
+        addVertex(position: float3( 1, 1,0), color: float4(1,0,0,1), normal: float3( 0.0, 0.0, -1.0), textureCoordinate: float2(1,0))
+        
+        //Top Left
+        addVertex(position: float3(-1, 1,0), color: float4(0,1,0,1), normal: float3( 0.0, 0.0, -1.0), textureCoordinate: float2(0,0))
+        
+        //Bottom Left
+        addVertex(position: float3(-1,-1,0), color: float4(0,0,1,1), normal: float3( 0.0, 0.0, -1.0), textureCoordinate: float2(0,1))
+        
+        //Top Right
+        addVertex(position: float3( 1, 1,0), color: float4(1,0,0,1), normal: float3( 0.0, 0.0, -1.0), textureCoordinate: float2(1,0))
+        
+        //Bottom Left
+        addVertex(position: float3(-1,-1,0), color: float4(0,0,1,1), normal: float3( 0.0, 0.0, -1.0), textureCoordinate: float2(0,1))
+        
+        //Bottom Right
+        addVertex(position: float3( 1,-1,0), color: float4(1,0,1,1), normal: float3( 0.0, 0.0, -1.0), textureCoordinate: float2(1,1))
+    }
+}
---
+++ The Dark Place/Libraries/New Group/CustomMeshes/Triangle_CustomMesh.swift
@@ -0,0 +1,9 @@
+import MetalKit
+
+class Triangle_CustomMesh: CustomMesh {
+    override func createVertices() {
+        addVertex(position: float3( 0, 1,0), color: float4(1,0,0,1))
+        addVertex(position: float3(-1,-1,0), color: float4(0,1,0,1))
+        addVertex(position: float3( 1,-1,0), color: float4(0,0,1,1))
+    }
+}
